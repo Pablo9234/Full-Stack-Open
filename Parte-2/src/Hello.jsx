5890348f2221221c09de5d6b4f88cc2a83057614
Renukta-21 <edu211004@gmail.com>
2024-06-26 13:54:24 -0600

    Refactoring

--- Parte-2/src/Hello.jsx
+++ Parte-2/src/Hello.jsx
@@ -12,55 +12,45 @@ export function Notes() {
         "Programming without an extremely heavy use of console.log is same as if a doctor would refuse to use x-rays or blood tests when diagnosing patients.",
         "The only way to go fast, is to go well.",
     ];
-
-    const initialVotes = anecdotes.map(() => ({ good: 0, neutral: 0, bad: 0})); //Creo un arreglo de objetos para poder tener los votos de cada nota de manera individual
     const [note, setNote] = useState(0); //Cambia el estado segun la nota que este seleccionada
-    const [votes,setVotes] = useState(initialVotes); //Lleva el control de cada voto en cada seccion del estado
-
-
+    const [votes, setVotes] = useState(new Array(anecdotes.length).fill(0)); //Lleva el control de cada voto en cada seccion del estado
 
     const handleLike = () => {
-        const newVotes = votes.map((vote, index) => 
-            index === note ? { ...vote, good: vote.good + 1 } : vote
-        );
-        setVotes(newVotes);
-    };
+        const newVotes = [...votes]
+        newVotes[note] = newVotes[note] + 1
+        setVotes(newVotes)
+    }
+    const mostVoted = () => {
+        let maxVotes = -1;
+        let maxIndex = -1;
+
+        votes.forEach((vote, index) => {
+            if (vote > maxVotes) {
+                maxVotes = vote;
+                maxIndex = index;
+            }
+        });
+
+        return maxIndex;
+    }
 
     const randonNumber = () => {
         const aleatorio = Math.floor(Math.random() * anecdotes.length)  //Ayuda a generar un numero aleatorio para poder cambiar de nota
         setNote(aleatorio)
     }
 
-    const allAVotes = () => {
-        let maxVotes = 0;
-        let mostVotedIndex = 0;
-
-        votes.forEach((vote, index) => {
-            const totalVotes = vote.good;
-            if (totalVotes > maxVotes) {
-                maxVotes = totalVotes;
-                mostVotedIndex = index;
-            }
-        });
-
-        return anecdotes[mostVotedIndex];
-    };
-    
-
-    return <>
-    <div className="card">
-    <h1>Give feedback</h1>
-    <p>
-        {anecdotes[note]}
-    </p>
-    <div className="feedback">
-    <button onClick={randonNumber}>Next Note</button> {/*Da paso a la siguiente nota */}
-        <button onClick={handleLike}>Good</button>
-            <h2>More voted</h2>
-            <p>{allAVotes()}</p>
-            Votes {votes[note].good}
+    return (
+        <div className="card">
+            <h1>Give feedback</h1>
+            <p>{anecdotes[note]} <b>Votes: {votes[note]}</b></p>
+            <div className="feedback">
+                <button onClick={randonNumber}>Next Note</button> {/* Da paso a la siguiente nota */}
+                <button onClick={handleLike}>Good</button>
+                <h2>Most voted</h2> {/* Corrección ortográfica */}
+                <div>
+                    <p>{anecdotes[mostVoted()]}</p>
+                </div>
             </div>
-    </div>
-     
-    </>
+        </div>
+    )
 }
